29e22486d5ef75ad9e24f13348d334445f2e84aa
mterracina <mterracina@wisc.edu>
2019-05-01 12:45:07 -0500

    new method for Choice - getIsCorrect_Bool(), also comments and updates

--- application/Choice.java
+++ application/Choice.java
@@ -27,10 +27,17 @@ package application;
 public class Choice implements ChoiceADT {
 
   // ************************** FIELDS ******************************************//
-  public String isCorrect;
-  public String choice;
+  public String isCorrect; // represents whether this Choice is the correct choice for the
+                           // associated answer ("T" - true, "F" - false)
+  public String choice; // represents the choice the user will see in the quiz
 
   // ************************** CONSTRUCTORS **************************************//
+  /**
+   * Initializes the instance of this Choice object
+   * 
+   * @param isCorrect - "T" if this is the correct choice for the associated Question, otherwise "F"
+   * @param choice - String representation for this choice for the associated Question
+   */
   Choice(String isCorrect, String choice) {
     this.isCorrect = isCorrect;
     this.choice = choice;
@@ -54,5 +61,18 @@ public class Choice implements ChoiceADT {
    */
   public String getIsCorrect() {
     return this.isCorrect;
+  }
+
+  /**
+   * Returns true or false depending on whether or not this Choice is the correct Choice for the
+   * associated Question
+   * 
+   * @return true if this Choice is the correct Choice, false otherwise
+   */
+  public boolean getIsCorrect_Bool() {
+    if (this.isCorrect.equals("T")) {
+      return true;
+    }
+    return false;
   }
 } // END CLASS
--- application/Question.java
+++ application/Question.java
@@ -1,11 +1,28 @@
-/**
- * 
- */
+///////////////////////////////////////////////////////////////////////////////
+//
+// Title:           Question
+// Course:          CS400 Spring 2019
+// Project:         Team Project 
+//
+// Author:          Mikel Terracina
+// Email:           mterracina@wisc.edu
+//
+// Lecturer's Name: Andrew Kuemmel
+// Lecture Number:  004
+//
+// Due Date:        2019-05-02
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+// Online Sources: none
+// 
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 package application;
 
 import java.util.List;
 
 /**
+ * This class represents a Question within the Quiz
+ * 
  * @author mterracina
  *
  */
@@ -17,24 +34,21 @@ public class Question implements QuestionADT {
   private String topic;
   private String image;
   private List<Choice> choices;
-//  private String answer;
 
   // ************************** CONSTRUCTORS **************************************//
-//  Question(String topic, String question, String answer, List<Choice> choices)  {
-  Question(String topic, String question, List<Choice> choices)  {
+  Question(String topic, String question, List<Choice> choices) {
     this.question = question;
     this.topic = topic;
-//    this.answer = answer;
     this.choices = choices; // max number of choices = 5
   }
-  
-  Question(String topic, String question, List<Choice> choices, String metadata)  {
+
+  Question(String topic, String question, List<Choice> choices, String metadata) {
     this.metadata = metadata;
     this.question = question;
     this.topic = topic;
     this.choices = choices; // max number of choices = 5
   }
- 
+
   Question(String topic, String question, List<Choice> choices, String metadata, String image) {
     this.metadata = metadata;
     this.question = question;
@@ -44,17 +58,31 @@ public class Question implements QuestionADT {
   }
 
   // ************************** METHODS ********************************************//
+  /**
+   * Returns the String representation of the actual question
+   * 
+   * @return question
+   */
   public String getQuestion() {
     return question;
   }
 
+  /**
+   * Returns the available choices a user can pick for an answer
+   * 
+   * @returns List of Choice objects for this Question
+   */
   public List<Choice> getChoices() {
     return choices;
   }
 
+  /**
+   * Returns the correct answer for this Question. If not found, returns null
+   * 
+   * @return correct answer in String format
+   */
   public String getAnswer() {
     for (Choice choice : choices) {
-      System.out.println("choices " + choice.getChoice() + "--" + choice.getIsCorrect());
       if (choice.getIsCorrect().equals("T")) {
         return choice.getChoice();
       }
@@ -62,14 +90,29 @@ public class Question implements QuestionADT {
     return null;
   }
 
+  /**
+   * Returns the metadata for this Question (i.e. miscellaneous information about the question)
+   * 
+   * @return metadata in String format
+   */
   public String getMetadata() {
     return metadata;
   }
 
+  /**
+   * Returns the topic that this question is associated with
+   * 
+   * @return topic in String format
+   */
   public String getTopic() {
     return topic;
   }
 
+  /**
+   * Returns the name of the image file that is associated with this question
+   * 
+   * @return name of the image file in String format
+   */
   public String getImage() {
     return image;
   }
