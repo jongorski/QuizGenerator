1a123e377662c01e47bf5c7ab63ed914cdd2d7ee
jackhwolf <jwolf22@wisc.edu>
2019-05-01 17:06:57 -0500

    ask

--- application/AskQuestion.java
+++ application/AskQuestion.java
@@ -19,6 +19,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -38,16 +39,7 @@ public class AskQuestion {
 	private int num_correct;
 	private int total_num;
 	private Label is_correct;
-	
-	
-	/*
-	 * constructor method for AskQuestion class
-	 * 
-	 * @param curr_stage: current stage, used to change scene
-	 * @param qb: QuizBank to quiz out of
-	 * @param topic: topic of quiz questions to ask
-	 * @param num_t0_asl: number of questions to ask
-	 */
+
 	public AskQuestion(Stage curr_stage, QuizBank qb, String topic, int requested_size) {
 		System.out.println("AskQuestion constr 1");
 		this.curr_stage = curr_stage;
@@ -72,70 +64,55 @@ public class AskQuestion {
 				resultsHandler();
 			}
 		});
-		
+
 		this.is_correct = new Label("");
 	}
-	
+
 	private void nextHandler() {
 		System.out.println("nextHandler 1");
 		Scene next_q_scene = null;
-		if (this.num_taken != this.total_num-1) {
+		if (this.num_taken != this.total_num - 1) {
 			System.out.println("nextHandler 2.a");
-			next_q_scene = AskQScene(this.selected_questions.get(this.num_taken),
-					                 this.next_button);
-		}
-		else {
+			next_q_scene = AskQScene(this.selected_questions.get(this.num_taken), this.next_button);
+		} else {
 			System.out.println("nextHandler 2.b");
-			next_q_scene = AskQScene(this.selected_questions.get(this.num_taken),
-	                 this.results_button);
+			next_q_scene = AskQScene(this.selected_questions.get(this.num_taken), this.results_button);
 		}
 		System.out.println("nextHandler 3");
 		this.num_taken++;
 		this.curr_stage.setScene(next_q_scene);
 	}
 	
-
-public Scene AskQScene(Question q, Button button) {
-		VBox main_vbox = new VBox();
-		HBox mid_hbox = new HBox(); // main hbox
-		VBox options_vbox = new VBox();
-		VBox img_details_button_vbox = new VBox();
-		
-		this.is_correct.setVisible(false);
-
-		// title - top of main vbox
-		Label title = new Label("Question " + this.num_taken + 1 + ":");
-		title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
-		VBox.setMargin(title, new Insets(20, 0, 40, 0));
-		main_vbox.getChildren().add(title);
-
-		// text of question - 2nd from top of main vbox
-		Label question_text = new Label(q.getQuestion());
-		question_text.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
-		VBox.setMargin(question_text, new Insets(20, 0, 40, 0));
-		main_vbox.getChildren().add(question_text);
-
-
-		// gets list of choices for questions
-		List<Choice> choicesList = q.getChoices();
-
-		// create radio buttons for multiple choice
-		// add to radio buttons list
+	private HBox midHbox(VBox left, VBox right) {
+		HBox hb = new HBox(15);
+		hb.setAlignment(Pos.CENTER);
+		hb.getChildren().addAll(left, right);
+		return hb;
+	}
+	
+	private VBox rbVbox(List<Choice> choices) {
+		VBox vb = new VBox(10);
 		List<RadioButton> rbs = new ArrayList<RadioButton>();
-		for (Choice c : choicesList) {
+		ToggleGroup group = new ToggleGroup();
+		for (Choice c : choices) {
 			RadioButton rb = new RadioButton(c.choice);
+			rb.setToggleGroup(group);
+			rb.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
 			rb.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent e) {
-					radioButtonHandler(rbs, choicesList);
+					radioButtonHandler(rbs, choices);
 				}
 			});
 			rbs.add(rb);
-			VBox.setMargin(rb, new Insets(10, 0, 10, 0));
-			options_vbox.getChildren().add(rb);
+			VBox.setMargin(rb, new Insets(10, 10, 10, 10));
+			vb.getChildren().add(rb);
 		} // rbs is now list of N buttons with labels 0,...,N-1
-
-
+		return vb;
+	}
+	
+	private VBox detailsVbox(Question q, Button button) {
+		VBox vb = new VBox(10);
 		// image display...
 		// https://www.tutorialspoint.com/javafx/javafx_images.htm
 		// Loading image from URL if image associated with question (not empty string)
@@ -151,50 +128,77 @@ public Scene AskQScene(Question q, Button button) {
 				imageView.setFitHeight(250);
 				// Setting the preserve ratio of the image view
 				imageView.setPreserveRatio(true);
-				img_details_button_vbox.getChildren().add(imageView);
+				vb.getChildren().add(imageView);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
-		// quiz completeness info
-		Label details = new Label(this.num_taken + 1 + " / " + this.total_num);
-		details.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
-		VBox.setMargin(details, new Insets(10, 0, 50, 0));
-		img_details_button_vbox.getChildren().add(details);
+		// quiz completeness info -- x/n
+		Label completeness = new Label(this.num_taken+1 + " / " + this.total_num);
+		completeness.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+		vb.getChildren().add(completeness);
 		
-		// correct/incorrect label
-		this.is_correct.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
-		VBox.setMargin(this.is_correct, new Insets(10, 0, 50, 0));
-		img_details_button_vbox.getChildren().add(this.is_correct);
+		this.is_correct.setText("Correct");
+		this.is_correct.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+		vb.getChildren().add(this.is_correct);
 		
-		mid_hbox.getChildren().add(options_vbox);
-		mid_hbox.getChildren().add(img_details_button_vbox);
+		return vb;
+	}
 
-		main_vbox.getChildren().add(mid_hbox);
+	public Scene AskQScene(Question q, Button button) {
+		VBox main_vb = new VBox(15);
+		main_vb.setAlignment(Pos.CENTER);
 		
-		// next/results button
+		//title - top of main_vb
+		int k = this.num_taken + 1;
+		Label title = new Label("Question " + k);
+		title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+		//VBox.setMargin(title, new Insets(20, 0, 40, 0));
+		main_vb.getChildren().add(title);
+		
+		//question text - 2nd to top of main_vb
+		Label text = new Label(q.getQuestion());
+		text.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+		//VBox.setMargin(text, new Insets(20, 0, 20, 0));
+		main_vb.getChildren().add(text);
+		
+		VBox rb_vb = rbVbox(q.getChoices());
+		VBox details = detailsVbox(q, button);
+		HBox mid_hb = midHbox(rb_vb, details);
+		
+		main_vb.getChildren().add(mid_hb);
+		
 		button.setStyle("-fx-font-size:30");
 		button.setMaxSize(300, 125);
 		button.setMinSize(300, 125);
-		
-		Scene scene = new Scene(main_vbox, 800, 700);
+		main_vb.getChildren().add(button);
+
+
+		Scene scene = new Scene(main_vb, 800, 700);
 		return scene;
 	}
 
-	
 	private void resultsHandler() {
-		if (this.num_taken > this.total_num) {
+		if (this.num_taken >= this.total_num) {
 			//DISPLAY FINAL WINDOWS OR CALL TO THAT CLASS, ETC
+		  QuizComplete quizComplete = new QuizComplete();
+		  quizComplete.QuizCompleteScene(curr_stage, num_correct, num_taken);
 		}
 	}
-	
+
 	public void beginQuiz() {
 		System.out.println("AskQuestion beginQuiz");
 		nextHandler();
 	}
-	
+
+	/**
+	 * If the radio button selected corresponds to the correct choice then it displays 
+	 * correct, else incorrect.
+	 * @param rbs
+	 * @param choices
+	 */
 	private void radioButtonHandler(List<RadioButton> rbs, List<Choice> choices) {
 		int k = 0;
 		for (int i = 0; i < rbs.size(); i++) {
@@ -210,5 +214,5 @@ public Scene AskQScene(Question q, Button button) {
 		}
 		this.is_correct.setVisible(true);
 	}
-	
+
 }
