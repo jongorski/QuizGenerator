6ba0582801ccae58829c12cf9dbc2696f4b692f7
jackhwolf <jwolf22@wisc.edu>
2019-05-03 15:43:48 -0500

    ask question

--- application/AskQuestion.java
+++ application/AskQuestion.java
@@ -34,21 +34,20 @@ public class AskQuestion {
 	private QuizBank qb;
 	private Button next_button;
 	private Button results_button;
-	private List<Question> selected_questions;
 	private int num_taken;
 	private int num_correct;
 	private int total_num;
 	private Label is_correct;
+	private List<Question> selected_questions;
 
-	public AskQuestion(Stage curr_stage, QuizBank quizbank, String topic, int requested_size) {
+	public AskQuestion(QuizBank quizbank, Stage curr_stage) {
 		this.curr_stage = curr_stage;
 		this.qb = quizbank;
-		this.selected_questions = qb.getRandomN(topic, requested_size);
 		this.next_button = new Button("Next Question");
 		this.results_button = new Button("Results");
 		this.num_taken = 0;
 		this.num_correct = 0;
-		this.total_num = this.selected_questions.size();
+		this.total_num = 0;
 		this.next_button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
@@ -61,10 +60,81 @@ public class AskQuestion {
 				resultsHandler();
 			}
 		});
-
 		this.is_correct = new Label("");
+		this.selected_questions = new ArrayList<Question>();
 	}
 
+	/**
+	 * method to generate scene for add question or
+	 * start menu
+	 * @return scene to show 
+	 */
+	public Scene ChooseTopicStart() {
+		VBox vbox = new VBox();
+		vbox.setAlignment(Pos.CENTER);
+
+		Scene scene = new Scene(vbox, 800, 700);
+
+		// Title of main page
+		Label title = new Label("QuizGenerator");
+		title.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+		VBox.setMargin(title, new Insets(20, 0, 70, 0));
+
+		// Start button
+		Button start_button = new Button("Start quiz!");
+		start_button.setStyle("-fx-font-size:30");
+		start_button.setMaxSize(300, 125);
+		start_button.setMinSize(300, 125);
+		VBox.setMargin(start_button, new Insets(70, 0, 40, 0));
+
+		// Topic list
+		int k = 0;
+		String[] topics = new String[this.qb.topics.size()];
+		for (String top : this.qb.topics) {
+			topics[k] = top;
+			k = k + 1;
+		}
+		ComboBox<String> topic_box = new ComboBox<String>(FXCollections.observableArrayList(topics));
+		topic_box.setVisibleRowCount(10);
+		topic_box.setPromptText("Topics");
+		topic_box.setStyle("-fx-font-size:15");
+		topic_box.setMinSize(225, 75);
+		topic_box.setMaxSize(225, 75);
+		topic_box.setPrefSize(225, 75);
+		VBox.setMargin(topic_box, new Insets(20, 0, 20, 0));
+
+		// Choose number of questions
+		TextField num_q = new TextField();
+		num_q.setPromptText("Enter # questions");
+		num_q.setStyle("-fx-font-size:15");
+		num_q.setMinSize(225, 75);
+		num_q.setMaxSize(225, 75);
+		VBox.setMargin(num_q, new Insets(20, 0, 20, 0));
+		
+		start_button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				startHandler((String)topic_box.getValue(), 
+						Integer.parseInt(num_q.getText()));
+			}
+		});
+
+		vbox.getChildren().add(title);
+		vbox.getChildren().add(topic_box);
+		vbox.getChildren().add(num_q);
+		vbox.getChildren().add(start_button);
+
+		return scene;
+	}
+	
+	private void startHandler(String topic, int num) {
+		for (int i=0; i<num && i<this.qb.table.get(topic).n; i++) {
+			this.selected_questions.add(this.qb.table.get(topic).get(i));
+		}
+		this.total_num = this.selected_questions.size();
+		nextHandler();
+	}
+	
 	private void nextHandler() {
 		Scene next_q_scene = null;
 		if (this.num_taken != this.total_num - 1) {
@@ -109,7 +179,7 @@ public class AskQuestion {
 		// image display...
 		// https://www.tutorialspoint.com/javafx/javafx_images.htm
 		// Loading image from URL if image associated with question (not empty string)
-		if (!q.getImage().equals("")) {
+		if (!q.getImage().equals("") && !q.getImage().contentEquals("none")) {
 			try {
 				// Creating an image
 				Image image = new Image(new FileInputStream(q.getImage()));
@@ -174,13 +244,12 @@ public class AskQuestion {
 
 	private void resultsHandler() {
 		if (this.num_taken >= this.total_num) {
-			// DISPLAY FINAL WINDOWS OR CALL TO THAT CLASS, ETC
 			QuizComplete quizComplete = new QuizComplete();
 			this.curr_stage.setScene(quizComplete.QuizCompleteScene(curr_stage, num_correct, num_taken, this.qb));
 		}
 	}
 
-	public void beginQuiz() {
+	private void START() {
 		nextHandler();
 	}
 
@@ -209,4 +278,8 @@ public class AskQuestion {
 		this.is_correct.setVisible(true);
 	}
 
+	public void beginQuiz() {
+		AskQuestion asker = new AskQuestion(this.qb, this.curr_stage);
+		curr_stage.setScene(asker.ChooseTopicStart());
+	}
 }
