65167578c262ae4969d58b0d759aa8e4c17839db
jackhwolf <jwolf22@wisc.edu>
2019-05-03 15:44:13 -0500

    main

--- application/Main.java
+++ application/Main.java
@@ -24,23 +24,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			/*
-			QuizBank qb = new QuizBank();
-			List<Choice> choices1 = new ArrayList<Choice>();
-			choices1.add(new Choice("T", "Harrier"));
-			choices1.add(new Choice("F", "Lab"));
-			Question q1 = new Question("dogs", "What breed is this?", 
-					choices1, "such a cute dog", "../"
-							+ "harrier-puppy.jpg");
-			List<Choice> choices2 = new ArrayList<Choice>();
-			choices2.add(new Choice("T", "yah"));
-			Question q2 = new Question("sports", "sports?", 
-					choices2, "sports", "");
-			qb.addQuestionToQuiz(q1);
-			qb.addQuestionToQuiz(q2);
-			*/
 			QuizStart.startQuiz(primaryStage);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
